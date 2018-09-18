e0226ef4ed66aef6ed4cf31ab83a8f75fe005360
BONITOUR <bonitour@iMac-4.local>
2018-09-18 13:20:36 -0300

    Alterando o projeto para ficar mais simples possível

--- contracts/Documents.sol
+++ contracts/Documents.sol
@@ -1,19 +1,37 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
-import "./zeppelin/owership/Ownable.sol";
-import "./Collaborator.sol";
+import "./zeppelin/ownership/Ownable.sol";
 
-contract Documents is Ownable, Collaborator {
+contract AcademicDocument is Ownable {
     mapping(address => Document) public _documents;
+    mapping(address => Authorizer) public _authorizers;
+    mapping(address => Student) public _student;
+    mapping(address => University) public _university;
+    
+    struct Authorizer {
+        address _address;
+        string name;
+    }
 
-    struct Document {
-        string hash;
-        string documenType;
-        uint entryDate;
-        mapping(address => uint8) student;
+    struct Student {
+        address _studentAddress;
+        string name;
+        string registrationId;
+        address university;
     }
     
-    function addBoletim(address _address, string hash) public onlyAuthorizer {
+    struct Document {
+        string hash;
+        string documentType;
+        uint entryDate;
+    }
+    
+    struct University {
+        address _universityAddress;
+        string name;
+    }
+    
+    function addDocument(address _address, string hash) public onlyAuthorizer {
         Document memory document;
         
         document.hash = hash;
@@ -22,7 +40,33 @@ contract Documents is Ownable, Collaborator {
         _documents[_address] = document;
     }
     
-    modifier onlyAuthorizer() {
-        _;
+    function addUniversity(address _address, string name) public onlyAuthorizer {
+        University memory university;
+        
+        university.name = name;
+        _university[_address] = university;
     }
+    
+    function addStudent(address _address, address university,  string name, string registrationId) public onlyAuthorizer {
+        Student memory student;
+        
+        student.name = name;
+        student.registrationId = registrationId;
+        student.university = university;
+        _student[_address] = student;
+    }
+    
+    function addAuthorizer(address _address, string name) public onlyOwner {
+        
+    }
+    
+    modifier onlyAuthorizer() {
+        require(_authorizers[msg.sender]._address != 0x0);
+        _;
+    }   
+    
+    // modifier onlyOwner() {
+    //     require(msg.sender == )
+    // }
 }
+
--- migrations/2_personIdentity.js
+++
@@ -1,6 +0,0 @@
-var PersonIdentity = artifacts.require("PersonIdentity");
-
-module.exports = async function(deployer, accounts) {
-	var advisor = accounts[0];
-	await deployer.deploy(PersonIdentity, "genesis", {from: advisor});
-}
