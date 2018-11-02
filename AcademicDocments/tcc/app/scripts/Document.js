2582d96238bfedfa92795bd69273ad29b791272f
Marcio Habigzang Brufatto <mhbrufatto@gmail.com>
2018-11-02 14:07:46 -0300

    Tentando fazer o contrato gerado funcionar

--- AcademicDocments/tcc/app/scripts/Document.js
+++ AcademicDocments/tcc/app/scripts/Document.js
@@ -21,8 +21,8 @@ $("#btnCadastrarDocumento").click(function () {
 	// 		console.log(e, contract);
 	// 		if (typeof contract.address !== 'undefined') {
 	// 			console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-	global = sessionStorage.getItem('contractAddress');
-	upload(global);
+	// global = sessionStorage.getItem('contractAddress');
+	upload(contractAddress);
 	// 		}
 	// 	})
 });
--- truffle/migrations/2_Document.js
+++ truffle/migrations/2_Document.js
@@ -1,5 +1,12 @@
+const write = require('write');
+const path = require('path');
+
 var Document = artifacts.require("./Document.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Document);
+  deployer.deploy(Document)
+    .then(_ => {
+      var content = `var contractAddress = '${Document.address}';`;
+      write(path.resolve(__dirname, '../../AcademicDocments/tcc/app/scripts/1_contract_address.js'), content);
+  })
 };
